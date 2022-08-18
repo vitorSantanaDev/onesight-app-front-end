7ce186840f83020de6d9a3905437c378c5fabb6e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-18 06:45:55 -0700

    feat: :sparkles: configuring api

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const APP_URL = process.env.REACT_APP_API_URL_LOCAL
+// const APP_URL = process.env.REACT_APP_API_URL_PRODUCTION
+
+const axiosInstance = axios.create({
+  baseURL: APP_URL
+})
+
+export default axiosInstance
