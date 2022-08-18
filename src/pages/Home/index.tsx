52719d940ac0b4f42b9258cdfa20f618618d9609
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-18 06:57:04 -0700

    feat: :sparkles: searching for appointments and rendering them on screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 
-import { Calendar } from '../../components'
+import { AppointmentCard, Calendar } from '../../components'
+import { AppointmentsContext } from '../../contexts/AppointmentsContext'
+import { ModalContext } from '../../contexts/ModalContext'
+
+import { AppointmentStatusEnum } from '../../interfaces/appointments'
+
+import { getAllAppointments } from '../../services/appointments.service'
 
 import * as S from './styles'
 
 const Home = () => {
+  const { showModal } = useContext(ModalContext)
+  const { appointments, setAppointments } = useContext(AppointmentsContext)
+
+  const fetchAllAppointments = () => {
+    ;(async () => {
+      const appointmentsData = await getAllAppointments()
+      setAppointments?.(appointmentsData.data.appointments)
+    })()
+  }
+
+  useEffect(fetchAllAppointments, [showModal, setAppointments])
+
   return (
     <S.HomePageWrapper>
       <Calendar />
+      <S.AppointmentsWrapper>
+        {appointments.length > 0 &&
+          appointments.map(({ name, description, date, status, _id }) => (
+            <AppointmentCard
+              key={_id}
+              ID={_id as string}
+              appointmentDate={date}
+              appointmentTitle={name}
+              appointmentDescription={description}
+              appointmentStatus={status as AppointmentStatusEnum}
+            />
+          ))}
+      </S.AppointmentsWrapper>
     </S.HomePageWrapper>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components'
 
-export const HomePageWrapper = styled.main``
+export const HomePageWrapper = styled.main`
+  width: 100%;
+`
+
+export const AppointmentsWrapper = styled.section`
+  gap: 30px;
+  display: grid;
+  padding: 32px 24px;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+
+  > a {
+    text-decoration: none;
+  }
+`
