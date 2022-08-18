9c474a03afe1fb03a81eed244382e8b0e3771f4e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-18 16:40:33 -0700

    style: :lipstick: improving details screen

--- src/components/AppointmentDetailComponent/index.tsx
+++ src/components/AppointmentDetailComponent/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { Fragment, useContext } from 'react'
 import { format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useNavigate } from 'react-router-dom'
+
+import { Edit } from '@styled-icons/material-outlined'
+
+import { ModalContext } from '../../contexts/ModalContext'
+
+import AppointmentCreationModal from '../AppointmentCreationModal'
+
+import { routesNameEnum } from '../../routes/routesEnum'
+import { AppointmentStatusEnum } from '../../interfaces/appointments'
 
 import { IAppointmentDetailComponentProps } from './types'
 
@@ -9,19 +19,69 @@ import * as S from './styles'
 const AppointmentDetailComponent = (
   props: IAppointmentDetailComponentProps
 ) => {
-  const { date, description, name, status } = props
+  const navigation = useNavigate()
+  const { showModal, setShowModal } = useContext(ModalContext)
+  const { date, description, name, status, _id } = props
+
+  const dateAppointmentFormated = String(
+    format(new Date(date), "iiii',' dd/MM/yyyy '-' p", { locale: ptBR })
+  )
+
+  const appointmentStatusValid = status as AppointmentStatusEnum
+
+  const iconColor =
+    appointmentStatusValid === AppointmentStatusEnum.APPROVED
+      ? '#1a1a1a'
+      : '#ffff'
+
+  const appointmentStatus =
+    status === AppointmentStatusEnum.APPROVED
+      ? 'Aceito'
+      : status === AppointmentStatusEnum.CANCELED
+      ? 'Cancelado'
+      : 'Em Aberto'
+
+  const goToHomeHandler = () => navigation(routesNameEnum.HOME)
+
+  const openModalToEditAppointmentHandler = () => {
+    setShowModal?.(!showModal)
+  }
 
   return (
-    <S.AppointmentDetailComponentWrapper>
-      <S.AppointmentTitle>{name}</S.AppointmentTitle>
-      <S.AppointmentDate>
-        {String(
-          format(new Date(date), "iiii',' dd/MM/yyyy '-' p", { locale: ptBR })
-        )}
-      </S.AppointmentDate>
-      <S.AppointmentDescription>{description}</S.AppointmentDescription>
-      <S.AppointmentStatus>{status}</S.AppointmentStatus>
-    </S.AppointmentDetailComponentWrapper>
+    <Fragment>
+      <AppointmentCreationModal
+        isItEditing
+        valueDate={date}
+        valueNameAppointment={name}
+        appointmentID={_id as string}
+        valueDescriptionAppointment={description}
+      />
+      <S.AppointmentDetailComponentWrapper status={appointmentStatusValid}>
+        <S.AppointmentTitle status={appointmentStatusValid}>
+          {name}
+        </S.AppointmentTitle>
+        <S.AppointmentDate status={appointmentStatusValid}>
+          {dateAppointmentFormated}
+        </S.AppointmentDate>
+        <S.AppointmentDescription status={appointmentStatusValid}>
+          {description}
+        </S.AppointmentDescription>
+        <S.StatusLabel status={appointmentStatusValid}>
+          Status - {appointmentStatus}
+        </S.StatusLabel>
+        <S.ButtonsActionsWrapper>
+          <S.ButtonActionGoToHome
+            onClick={goToHomeHandler}
+            status={appointmentStatusValid}
+          >
+            Voltar
+          </S.ButtonActionGoToHome>
+          <S.ButtonEditAppointment onClick={openModalToEditAppointmentHandler}>
+            <Edit size={24} color={iconColor} />
+          </S.ButtonEditAppointment>
+        </S.ButtonsActionsWrapper>
+      </S.AppointmentDetailComponentWrapper>
+    </Fragment>
   )
 }
 
--- src/components/AppointmentDetailComponent/styles.ts
+++ src/components/AppointmentDetailComponent/styles.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { AppointmentStatusEnum } from '../../interfaces/appointments'
 
-export const AppointmentDetailComponentWrapper = styled.div`
+type AppointmentStatus = {
+  status: AppointmentStatusEnum
+}
+
+export const AppointmentDetailComponentWrapper = styled.div<AppointmentStatus>`
   width: 100%;
   padding: 32px;
   max-width: 580px;
@@ -8,26 +13,128 @@ export const AppointmentDetailComponentWrapper = styled.div`
   background-color: #006edc;
 
   border-radius: 5px;
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.APPROVED &&
+    css`
+      color: #1b1b1b;
+      background-color: #03fc24;
+    `}
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.CANCELED &&
+    css`
+      background-color: #fc0352;
+    `}
 `
 
-export const AppointmentTitle = styled.h2`
+export const AppointmentTitle = styled.h2<AppointmentStatus>`
   color: #fff;
   font-size: 24px;
   margin-bottom: 16px;
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.APPROVED &&
+    css`
+      color: #1b1b1b;
+    `}
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.CANCELED &&
+    css`
+      text-decoration: line-through;
+    `}
 `
 
-export const AppointmentDate = styled.span`
-  color: #dbdbdb;
+export const AppointmentDate = styled.span<AppointmentStatus>`
+  color: #fff;
   font-weight: 600;
   font-size: 18px;
   margin-bottom: 16px;
   display: inline-block;
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.APPROVED &&
+    css`
+      color: #1b1b1b;
+    `}
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.CANCELED &&
+    css`
+      text-decoration: line-through;
+    `}
 `
 
-export const AppointmentDescription = styled.p`
+export const AppointmentDescription = styled.p<AppointmentStatus>`
   font-size: 16px;
-  color: #c4c2c2;
+  color: #e3e4e6;
   margin-bottom: 16px;
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.APPROVED &&
+    css`
+      color: #363433;
+    `}
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.CANCELED &&
+    css`
+      text-decoration: line-through;
+    `}
 `
 
-export const AppointmentStatus = styled.span``
+export const StatusLabel = styled.strong<AppointmentStatus>`
+  color: #ffff;
+  font-size: 16px;
+  margin-top: 10px;
+  display: inline-block;
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.APPROVED &&
+    css`
+      color: #1b1b1b;
+    `}
+`
+
+export const ButtonsActionsWrapper = styled.div`
+  width: 100%;
+  margin-top: 20px;
+
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+`
+
+export const ButtonActionGoToHome = styled.button<AppointmentStatus>`
+  color: #fff;
+  border: none;
+  font-size: 18px;
+  cursor: pointer;
+  text-decoration: underline;
+  background-color: transparent;
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.APPROVED &&
+    css`
+      color: #363433;
+    `}
+
+  ${({ status }) =>
+    status === AppointmentStatusEnum.CANCELED &&
+    css`
+      text-decoration: line-through;
+    `}
+
+  transition: ease-in-out 300ms all;
+
+  &:hover {
+    transform: scale(1.01);
+  }
+`
+
+export const ButtonEditAppointment = styled.button`
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+`
